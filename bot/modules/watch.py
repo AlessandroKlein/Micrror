0551f28ef14a9b433d429d08e33449b02f567e3e
Alessandro Klein <42942417+AlessandroKlein@users.noreply.github.com>
2021-08-16 15:35:46 -0300

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -17,11 +17,11 @@ def _watch(bot: Bot, update, isTar=False):
     try:
         link = message_args[1]
     except IndexError:
-        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
-        msg += "<b>Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
-        msg += "\n\nIf you want to use custom filename, enter it after |"
-        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
-        msg += "This file will be downloaded in 720p quality and it's name will be <b>Slam</b>"
+        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] para reflejar con youtube-dl.\n\n"
+        msg += "<b>Nota: la calidad y el nombre personalizado son opcionales</b>\n\nEjemplo de calidad: audio, 144, 240, 360, 480, 720, 1080, 2160."
+        msg += "\n\nSi desea utilizar un nombre de archivo personalizado, ingréselo después |"
+        msg += f"\n\nEjemplo:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
+        msg += "Este archivo se descargará en calidad 720p y su nombre será <b>Slam</b>"
         sendMessage(msg, bot, update)
         return
     
