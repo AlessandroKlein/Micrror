bf90bbadd26ec11948e9550bdc97ac4ab45c2947
Alessandro Klein <42942417+AlessandroKlein@users.noreply.github.com>
2021-08-16 17:08:47 -0300

    Update db_handler.py

--- bot/helper/ext_utils/db_handler.py
+++ bot/helper/ext_utils/db_handler.py
@@ -11,7 +11,7 @@ class DbManger:
             self.conn = psycopg2.connect(DB_URI)
             self.cur = self.conn.cursor()
         except psycopg2.DatabaseError as error :
-            LOGGER.error("Error in dbMang : ", error)
+            LOGGER.error("Error en dbMang : ", error)
             self.err = True
 
     def disconnect(self):
@@ -21,7 +21,7 @@ class DbManger:
     def db_auth(self,chat_id: int):
         self.connect()
         if self.err :
-            return "There's some error check log for details"
+            return "Hay un registro de verificación de errores para obtener más detalles"
         else:
             sql = 'INSERT INTO users VALUES ({});'.format(chat_id)
             self.cur.execute(sql)
@@ -33,19 +33,19 @@ class DbManger:
     def db_unauth(self,chat_id: int):
         self.connect()
         if self.err :
-            return "There's some error check log for details"
+            return "Hay un registro de verificación de errores para obtener más detalles"
         else:
             sql = 'DELETE from users where uid = {};'.format(chat_id)
             self.cur.execute(sql)
             self.conn.commit()
             self.disconnect()
             AUTHORIZED_CHATS.remove(chat_id)
-            return 'Unauthorized successfully'
+            return 'No autorizado con éxito'
 
     def db_addsudo(self,chat_id: int):
         self.connect()
         if self.err :
-            return "There's some error check log for details"
+            return "Hay un registro de verificación de errores para obtener más detalles"
         else:
             if chat_id in AUTHORIZED_CHATS:
                 sql = 'UPDATE users SET sudo = TRUE where uid = {};'.format(chat_id)
@@ -53,23 +53,23 @@ class DbManger:
                 self.conn.commit()
                 self.disconnect()
                 SUDO_USERS.add(chat_id)
-                return 'Successfully promoted as Sudo'
+                return 'Exitosamente promovido como Sudo'
             else:
                 sql = 'INSERT INTO users VALUES ({},TRUE);'.format(chat_id)
                 self.cur.execute(sql)
                 self.conn.commit()
                 self.disconnect()
                 SUDO_USERS.add(chat_id)
-                return 'Successfully Authorized and promoted as Sudo'
+                return 'Autorizado y promocionado con éxito como Sudo'
 
     def db_rmsudo(self,chat_id: int):
         self.connect()
         if self.err :
-            return "There's some error check log for details"
+            return "Hay un registro de verificación de errores para obtener más detalles"
         else:
             sql = 'UPDATE users SET sudo = FALSE where uid = {};'.format(chat_id)
             self.cur.execute(sql)
             self.conn.commit()
             self.disconnect()
             SUDO_USERS.remove(chat_id)
-            return 'Successfully removed from Sudo'
+            return 'Eliminado con éxito de Sudo'
